3a76124dee51e02cf1d1b842bdcdb788a10f6657
Klient <klientcos@gmail.com>
2021-04-19 16:45:27 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SearchBox from './SearchBox';
 import { robots } from './robots';
 
 class App extends Component {
+
   constructor() {
     super()
     this.state = {
@@ -12,22 +13,16 @@ class App extends Component {
     }
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
+  
 
   render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
     return (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <CardList robots={filteredRobots} />
-        </div>
-    )
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={this.state.robots} />
+      </div>
+    );
   }
 }
 
