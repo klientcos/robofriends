96e61f6c2a9522f7a828f517c585bcee5938bee7
Klient <klientcos@gmail.com>
2021-04-21 15:24:47 -0700

    just updates

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 
@@ -26,16 +27,21 @@ class App extends Component {
   }
 
   render() {
-      const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
+      const { robots, searchfield } = this.state;
+      const filteredRobots = robots.filter(robot => {
+      return robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase())
     })
 
-    return(
+    return !robots.length?
+    <h1> Loading </h1> :
+    (
       <div className='tc'>
          <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
-            <CardList robots={filteredRobots} />  
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />  
+            </ErrorBoundary>
           </Scroll>
       </div>
     );
