87fb6352f1048816d720a55449eabee89c462c91
Klient <klientcos@gmail.com>
2021-04-21 11:57:39 -0700

    updated scroll

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 
@@ -33,9 +34,9 @@ class App extends Component {
       <div className='tc'>
          <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
-          <scroll>
+          <Scroll>
             <CardList robots={filteredRobots} />  
-          </scroll>
+          </Scroll>
       </div>
     );
   }
